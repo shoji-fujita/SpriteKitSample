3e039c45eb603e7042add2f205aadf23736a01c4
shoji-fujita <shoji.fujita.0819@gmail.com>
2017-03-07 01:17:14 +0900

    first commit

---
+++ SpriteKitSample/Enum+Iterator.swift
@@ -0,0 +1,9 @@
+//
+//  Enum+Iterator.swift
+//  SpriteKitSample
+//
+//  Created by shoji on 2017/03/07.
+//  Copyright © 2017年 shoji fujita. All rights reserved.
+//
+
+import Foundation
---
+++ SpriteKitSample/SKEmitterNode+FileName.swift
@@ -0,0 +1,9 @@
+//
+//  SKEmitterNode+FileName.swift
+//  SpriteKitSample
+//
+//  Created by shoji on 2017/03/07.
+//  Copyright © 2017年 shoji fujita. All rights reserved.
+//
+
+import Foundation
